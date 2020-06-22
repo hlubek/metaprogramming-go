f482ef0415abd628f207e8d929a7c0901d47dc8e
Christopher Hlubek <hlubek@networkteam.com>
2020-06-22 18:38:23 +0200

    Code Generator Part 3

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -5,22 +5,18 @@ import (
 
 	"github.com/gofrs/uuid"
 
-	"github.com/hlubek/metaprogramming-go/domain"
 	"github.com/hlubek/metaprogramming-go/repository"
 )
 
 func main() {
 	ctx := context.Background()
 
-	repository.InsertProduct(ctx, nil, domain.Product{
-		ID:                uuid.Must(uuid.NewV4()),
-		ArticleNumber:     "12345678",
-		Name:              "Cheddar cheese",
-		Description:       "Very cheesy.",
-		Color:             "orange",
-		Size:              "large",
-		StockAvailability: 3,
-		PriceCents:        469,
-		OnSale:            false,
+	repository.UpdateProduct(ctx, nil, uuid.Must(uuid.FromString("b34081c7-9f33-4b04-ba33-3a112199f8c2")), repository.ProductChangeSet{
+		ArticleNumber: stringPtr("12345678"),
+		Name:          stringPtr("Cheddar cheese"),
 	})
+}
+
+func stringPtr(s string) *string {
+	return &s
 }
--- repository/mapping_product_gen.go
+++ repository/mapping_product_gen.go
@@ -14,3 +14,35 @@ type ProductChangeSet struct {
 	PriceCents        *int
 	OnSale            *bool
 }
+
+func (c ProductChangeSet) toMap() map[string]interface{} {
+	m := make(map[string]interface{})
+	if c.ID != nil {
+		m["product_id"] = *c.ID
+	}
+	if c.ArticleNumber != nil {
+		m["article_number"] = *c.ArticleNumber
+	}
+	if c.Name != nil {
+		m["name"] = *c.Name
+	}
+	if c.Description != nil {
+		m["description"] = *c.Description
+	}
+	if c.Color != nil {
+		m["color"] = *c.Color
+	}
+	if c.Size != nil {
+		m["size"] = *c.Size
+	}
+	if c.StockAvailability != nil {
+		m["stock_availability"] = *c.StockAvailability
+	}
+	if c.PriceCents != nil {
+		m["price_cents"] = *c.PriceCents
+	}
+	if c.OnSale != nil {
+		m["on_sale"] = *c.OnSale
+	}
+	return m
+}
